c63c0216aa413edd51f1bf3d25617efc363cbde7
rivannugroho <mhdrivannugroho@gmail.com>
2023-12-19 23:15:02 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     }else{
         header.classList.remove('navbar-fixed')
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -18,4 +23,65 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click' , function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+/// klik di luar
+window.addEventListener('click', function (e) {
+    if(e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+    }
+});
+
+/// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click' , function (){
+    if(darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+//pndahkan posisi toggel sesuai mode
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
+////////////////////////////////////////////////////////////////
+
+const textArray = ["Muhammad Rivan Nugroho"];
+let textIndex = 0;
+let charIndex = 0;
+const typingText = document.getElementById("typing-text");
+
+function type() {
+  if (charIndex < textArray[textIndex].length) {
+    typingText.textContent += textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = textArray[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    textIndex = (textIndex + 1) % textArray.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, 1000);
 });
